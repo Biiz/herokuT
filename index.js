c8dfe7bee3af5dca3157cf98fb91a9e45860b43c
Biiz <bizzo174@gmail.com>
2016-12-08 11:56:45 +0100

    asd

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.get('/', function(request, response) 
+{
+	
+	data= fs.readFile('/index.html',   function (err, data) {
+    //Other information required...
+    res.setHeader('Content-Type', 'text/html');
+    res.send(data);
+  	
+});
 
-
-app.use(express.static(__dirname + '/index.html', { redirect : false }));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
