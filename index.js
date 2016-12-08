1f6e14479f5b8cd58ce5613fbffb9b75ea704f2b
Biiz <bizzo174@gmail.com>
2016-12-08 11:48:05 +0100

    ad

--- index.js
+++ index.js
@@ -5,15 +5,9 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(request, response) 
-{
 
-    
-	response.redirect('/index.html');
-  	response.end();
-  	
-});
 
+app.use(express.static(herokuT + '/index.html', { redirect : false }));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
